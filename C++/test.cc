d51bdaa778476e8503d58b64cb6c378979bfcc41
wangyibo <1156873578@qq.com>
2022-12-14 09:19:40 +0800

    1

--- C++/test.cc
+++ C++/test.cc
@@ -4,19 +4,20 @@ static int &f = i;
 int &demo_static(int **addr)
 {
     *addr = &i;
-    printf("demo_static： i 的地址: %p, i=%d\n", &i, i);
-
-    return i;
+    printf("demo_static: i 的地址: %p, i=%d\n", &i, i);
+    int a = 5;
+    int &aa = a;
+    return aa;
 }
 int main(int argc, char **argv)
 {
     int a = 0, b = 0;
-    int & aaa = a;
-    int & bb = aaa;
+    int &aaa = a;
+    int &bb = aaa;
     std::cout << bb << std::endl;
     int *aa = &a;
 
-    ++demo_static(&aa);
+    f = demo_static(&aa);
 
     std::cout << f << std::endl;
 }
